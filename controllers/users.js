3553fbe14470d7525dff59699773a6c4dcc3ff86
Denis S Dujota <denisdujota@gmail.com>
2024-08-13 13:03:06 +0300

    setup signin

--- controllers/users.js
+++ controllers/users.js
@@ -24,4 +24,26 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/signin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const existingUser = await User.findOne({ username });
+
+    if (!existingUser) {
+      return res.status(400).json({ error: 'Invalid Credentials' });
+    }
+
+    const isValidPassword = bcrypt.compareSync(password, existingUser.hashedPassword);
+
+    if (!isValidPassword) {
+      throw Error('Invalid Credentials');
+    }
+
+    return res.status(200).json({ user: existingUser });
+  } catch (error) {
+    res.status(400).json({ error: 'Something went wrong, try again.' });
+  }
+});
+
 module.exports = router;
