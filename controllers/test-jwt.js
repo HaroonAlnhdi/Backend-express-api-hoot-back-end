969e05120422784d0b178f386efaa7ce6c28f549
Denis S Dujota <denisdujota@gmail.com>
2024-08-13 10:45:58 +0300

    setup base jwt controller:

---
+++ controllers/test-jwt.js
@@ -0,0 +1,11 @@
+// controllers/test-jwt.js
+
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/sign-token', (req, res) => {
+  res.json({ message: 'You are authorized!' });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
 dotenv.config();
+
 require('./config/database');
 const express = require('express');
+
+// Controllers
+const testJWTRouter = require('./controllers/test-jwt');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
+app.use('/test-jwt', testJWTRouter);
 
 app.listen(PORT, () => {
   console.log('The express app is ready!');
