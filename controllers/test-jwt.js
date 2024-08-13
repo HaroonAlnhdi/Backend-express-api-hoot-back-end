7b527a117f3f988886564a2954350e5f6ef00ba5
Denis S Dujota <denisdujota@gmail.com>
2024-08-13 11:29:43 +0300

    setup verify and sign

--- controllers/test-jwt.js
+++ controllers/test-jwt.js
@@ -1,11 +1,28 @@
-// controllers/test-jwt.js
+const MOCK_USER = {
+  _id: 1,
+  username: 'test',
+  password: 'test',
+};
 
+const jwt = require('jsonwebtoken');
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/sign-token', (req, res) => {
-  res.json({ message: 'You are authorized!' });
+  const token = jwt.sign(MOCK_USER, process.env.JWT_SECRET);
+  res.json({ token });
 });
 
+router.post('/verify-token', (req, res) => {
+  try {
+    const token = req.headers.authorization.split(' ')[1];
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    res.json({ decoded });
+  } catch (err) {
+    res.status(401).json({ message: 'Invalid token' });
+  }
+});
 module.exports = router;
